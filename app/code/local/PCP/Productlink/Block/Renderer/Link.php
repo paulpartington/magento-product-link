efe8d43501946cc71f894a384027a372d43021a2
= <paulpartington26@gmail.com>
2012-04-14 16:39:06 +0100

    Set link text to say 'view'.

--- app/code/local/PCP/Productlink/Block/Renderer/Link.php
+++ app/code/local/PCP/Productlink/Block/Renderer/Link.php
@@ -8,7 +8,7 @@ class PCP_Productlink_Block_Renderer_Link extends Mage_Adminhtml_Block_Widget_Gr
         $data = '';
 
         if ( $_product->getUrlPath() ) {
-            $data = '<a href="' . Mage::getUrl($_product->getUrlPath()) . '" target="_blank">' . $this->__('Link') . '</a>';
+            $data = '<a href="' . Mage::getUrl($_product->getUrlPath()) . '" target="_blank">' . $this->__('View') . '</a>';
         }
 
         return $data;
